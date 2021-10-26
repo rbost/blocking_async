c3337271ed31b1c19d8da0921027bd6c051862f7
Raphael Bost <raphael_bost@alumni.brown.edu>
2021-10-26 22:51:32 +0200

    Reduce StreamIter's state. We use a state machine instead. It merely uses an `unsafe` in order to pin a variable on the stack.

--- src/iter_stream.rs
+++ src/iter_stream.rs
@@ -1,4 +1,5 @@
 #![allow(unused_imports)]
+use core::panic;
 use std::ops::DerefMut;
 use std::pin::Pin;
 use std::sync::RwLock;
@@ -7,15 +8,20 @@ use std::task::{Context, Poll};
 use core::iter::Iterator;
 
 use futures::channel::oneshot::Receiver;
+use futures::future::Pending;
 use futures::pin_mut;
 use futures::{Future, Stream, StreamExt};
 
 use crate::hybrid_mpsc;
 use crate::utils;
 
+enum StreamIterState<I, V> {
+    Iterator(I),
+    Receiver(Receiver<(Option<V>, I)>),
+}
+
 pub struct StreamIter<I, V> {
-    iter: Option<I>,
-    state: Option<Receiver<(Option<V>, I)>>,
+    state: Option<StreamIterState<I, V>>,
 }
 
 impl<I, V> Unpin for StreamIter<I, V> {}
@@ -29,8 +35,7 @@ where
     let iter = i.into_iter();
 
     utils::assert_stream::<I::Item, _>(StreamIter {
-        iter: Some(iter),
-        state: None,
+        state: Some(StreamIterState::Iterator(iter)),
     })
 }
 
@@ -42,40 +47,52 @@ where
     type Item = I::Item;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<I::Item>> {
-        match &mut self.state {
-            Some(receiver) => {
-                pin_mut!(receiver);
-                receiver.poll(cx).map(|res| match res {
-                    Err(_) => {
+        let state = self.state.take();
+
+        match state {
+            None => panic!("Invalid StreamIter state"),
+
+            Some(StreamIterState::Receiver(mut receiver)) => {
+                // Why not using `pin_mut!` here?
+                // We need to reuse `receiver` is case the future is pending, so we cannot move it inside the macro.
+                // SAFETY: It is ok to use `new_unchecked` as receiver cannot get dropped while pinned. Indeed, it stays on the stack.
+                let pined_receiver = unsafe { Pin::new_unchecked(&mut receiver) };
+                let status = pined_receiver.poll(cx);
+
+                match status {
+                    Poll::Pending => {
+                        self.state = Some(StreamIterState::Receiver(receiver));
+                        Poll::Pending
+                    }
+                    Poll::Ready(Err(_)) => {
                         panic!("Unexpected oneshot cancel")
                     }
-                    Ok((v, iter)) => {
-                        self.state = None;
-                        self.iter = Some(iter);
-                        v
+                    Poll::Ready(Ok((v, iter))) => {
+                        self.state = Some(StreamIterState::Iterator(iter));
+                        Poll::Ready(v)
                     }
-                })
+                }
             }
-            None => {
+            Some(StreamIterState::Iterator(iter)) => {
                 let (sender, receiver) =
                     futures::channel::oneshot::channel::<(Option<Self::Item>, I)>();
 
-                let mut iter = self.iter.take().unwrap();
-
+                let mut iter = iter;
                 rayon::spawn(move || {
                     let v = iter.next();
                     let _ = sender.send((v, iter));
                 });
-                self.state = Some(receiver);
+                self.state = Some(StreamIterState::Receiver(receiver));
                 self.poll_next(cx)
             }
         }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        match &self.iter {
-            None => (0, None),
-            Some(iter) => iter.size_hint(),
+        match &self.state {
+            None => panic!("Invalid StreamIter state"),
+            Some(StreamIterState::Iterator(iter)) => iter.size_hint(),
+            _ => (0, None),
         }
     }
 }
